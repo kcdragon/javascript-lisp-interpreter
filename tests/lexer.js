dca3a6cac2af556510e13219b228f486e5d4d347
Mike Dalton <mdalton@weblinc.com>
2016-09-05 14:20:46 -0400

    moving Lexer to its own JS file

--- tests/lexer.js
+++ tests/lexer.js
@@ -1,18 +1,18 @@
-QUnit.test("Lexer#token single list", function(assert) {
+QUnit.test("Lexer#tokens single list", function(assert) {
     assert.deepEqual(
         ["(", "+", 1, 1, ")"],
         new Lexer("(+ 1 1)").tokens()
     )
 });
 
-QUnit.test("Lexer#token nested list", function(assert) {
+QUnit.test("Lexer#tokens nested list", function(assert) {
     assert.deepEqual(
         ["(", "-", "(", "+", 1, 1, ")", 1, ")"],
         new Lexer("(- (+ 1 1) 1)").tokens()
     )
 });
 
-QUnit.test("Lexer#token floating point number", function(assert) {
+QUnit.test("Lexer#tokens floating point number", function(assert) {
     assert.deepEqual(
         ["(", "+", 1.1, 1.2, ")"],
         new Lexer("(+ 1.1 1.2)").tokens()
