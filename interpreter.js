2ca145fa4c612886a63eb80bf47c9c28505e4ef7
Mike Dalton <mdalton@weblinc.com>
2016-09-06 19:50:30 -0400

    refactoring duplicate operand interpretation

--- interpreter.js
+++ interpreter.js
@@ -8,31 +8,29 @@ class Interpreter {
     }
 
     _result(expression) {
-        var interpreter = this
-
         if (!(expression instanceof Array)) {
             return expression
         }
 
         if (expression[0] == "+") {
-            return expression.slice(1).reduce(function (sum, operand) {
-                return sum + interpreter._result(operand)
+            return this._evaluateList(expression.slice(1)).reduce(function (sum, operand) {
+                return sum + operand
             }, 0)
         }
         else if (expression[0] == "-") {
-            return expression.slice(2).reduce(function (result, operand) {
-                return result - interpreter._result(operand)
-            }, interpreter._result(expression[1]))
+            return this._evaluateList(expression.slice(2)).reduce(function (result, operand) {
+                return result - operand
+            }, this._result(expression[1]))
         }
         else if (expression[0] == "*") {
-            return expression.slice(1).reduce(function (result, operand) {
-                return result * interpreter._result(operand)
+            return this._evaluateList(expression.slice(1)).reduce(function (result, operand) {
+                return result * operand
             }, 1)
         }
         else if (expression[0] == "/") {
-            return expression.slice(2).reduce(function (result, operand) {
-                return result / interpreter._result(operand)
-            }, interpreter._result(expression[1]))
+            return this._evaluateList(expression.slice(2)).reduce(function (result, operand) {
+                return result / operand
+            }, this._result(expression[1]))
         }
         else if (expression[0] == "eql") {
             return this._result(expression[1]) == this._result(expression[2])
@@ -40,5 +38,12 @@ class Interpreter {
         else {
             throw "Operation '" + expression[0] + "' is not supported"
         }
+    }
+
+    _evaluateList(expression) {
+        var interpreter = this
+        return expression.map(function (expression) {
+            return interpreter._result(expression)
+        })
     }
 }
