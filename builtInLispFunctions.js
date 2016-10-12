45b15d5784c2c444ead60ccb1e2b03878a3ff317
Mike Dalton <dalton@zoomer.io>
2016-10-11 23:00:10 -0400

    Moving built in Lisp functions to separate file

---
+++ builtInLispFunctions.js
@@ -0,0 +1,55 @@
+function newBuiltInLispFunctions() {
+  var builtInLispFunctions = {}
+  builtInLispFunctions["+"] = function (expression) {
+    return expression[0] + expression[1]
+  }
+  builtInLispFunctions["-"] = function (expression) {
+    return expression[0] - expression[1]
+  }
+  builtInLispFunctions["*"] = function (expression) {
+    return expression[0] * expression[1]
+  }
+  builtInLispFunctions["/"] = function (expression) {
+    return expression[0] / expression[1]
+  }
+  builtInLispFunctions["eql"] = function (expression) {
+    return expression[0] == expression[1]
+  }
+  builtInLispFunctions["and"] = function (expression) {
+    return expression[0] && expression[1]
+  }
+  builtInLispFunctions["or"] = function (expression) {
+    return expression[0] || expression[1]
+  }
+  builtInLispFunctions["not"] = function (expression) {
+    return !expression[0]
+  }
+  builtInLispFunctions["list"] = function (expression) {
+    return expression
+  }
+  builtInLispFunctions["car"] = function (expression) {
+    return expression[0][0]
+  }
+  builtInLispFunctions["cdr"] = function (expression) {
+    return expression[0].slice(1)
+  }
+  builtInLispFunctions["setq"] = function (expression, environment) {
+    environment[expression[0]] = new Interpreter([expression[1]], environment).result()
+  }
+  builtInLispFunctions["lambda"] = function (expression, environment) {
+    var lambdaArgumentNames = expression[0]
+
+    return function (args) {
+      var localEnvironment = _.clone(environment)
+
+      _.zip(lambdaArgumentNames, args).forEach(argumentNameValuePair => {
+        var argumentName = argumentNameValuePair[0]
+        var argumentValue = argumentNameValuePair[1]
+        localEnvironment[argumentName] = argumentValue
+      })
+
+      return new Interpreter([expression[1]], localEnvironment).result()
+    }
+  }
+  return builtInLispFunctions
+}
--- interpreter.js
+++ interpreter.js
@@ -1,58 +1,5 @@
-var builtInLispFunctions = {}
-builtInLispFunctions["+"] = function (expression) {
-  return expression[0] + expression[1]
-}
-builtInLispFunctions["-"] = function (expression) {
-  return expression[0] - expression[1]
-}
-builtInLispFunctions["*"] = function (expression) {
-  return expression[0] * expression[1]
-}
-builtInLispFunctions["/"] = function (expression) {
-  return expression[0] / expression[1]
-}
-builtInLispFunctions["eql"] = function (expression) {
-  return expression[0] == expression[1]
-}
-builtInLispFunctions["and"] = function (expression) {
-  return expression[0] && expression[1]
-}
-builtInLispFunctions["or"] = function (expression) {
-  return expression[0] || expression[1]
-}
-builtInLispFunctions["not"] = function (expression) {
-  return !expression[0]
-}
-builtInLispFunctions["list"] = function (expression) {
-  return expression
-}
-builtInLispFunctions["car"] = function (expression) {
-  return expression[0][0]
-}
-builtInLispFunctions["cdr"] = function (expression) {
-  return expression[0].slice(1)
-}
-builtInLispFunctions["setq"] = function (expression) {
-  builtInLispFunctions[expression[0]] = new Interpreter([expression[1]]).result()
-}
-builtInLispFunctions["lambda"] = function (expression, environment = builtInLispFunctions) {
-  var lambdaArgumentNames = expression[0]
-
-  return function (args) {
-    var localEnvironment = _.clone(environment)
-
-    _.zip(lambdaArgumentNames, args).forEach(argumentNameValuePair => {
-      var argumentName = argumentNameValuePair[0]
-      var argumentValue = argumentNameValuePair[1]
-      localEnvironment[argumentName] = argumentValue
-    })
-
-    return new Interpreter([expression[1]], localEnvironment).result()
-  }
-}
-
 class Interpreter {
-  constructor(expressions, environment = builtInLispFunctions) {
+  constructor(expressions, environment = newBuiltInLispFunctions()) {
     this.expressions = expressions
     this.environment = environment
   }
